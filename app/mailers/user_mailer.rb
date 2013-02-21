e03ae8ea5d1be8136bfdb744d5d0eeba74e48793
redxeagle <redxeag@googlemail.com>
2013-02-21 19:53:11 +0100

    add new email layouts

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,7 +11,7 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
       @user = user
-      @url  = "http://example.com/login"
-      mail(:to => user.email, :subject => "Welcome to My Awesome Site")
+      @url  = "http://#{APP_CONFIG['hostname']}"
+      mail(:to => user.email, :subject => "Willkommen bei Boulderevents.de")
     end
 end
